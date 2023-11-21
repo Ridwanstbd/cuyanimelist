3d370c2b2a52f752a5baa47658956a2cc45bee1c
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2023-11-21 16:54:57 +0700

    fixing

---
+++ src/app/populer/page.js
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <div>
+        <div>populer PAGE WITH ARROW FUNC</div>
+        <p>ini adalah search page</p>
+        </div>
+    )
+}
+export default Page;
